e789c0cf1c32de6dfd3ce6db91922bdacd40d44c
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-06-30 22:49:31 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT;
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const path = require('path')
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(errorHandler);
 
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 //server Frontend
-if(process.env.NODE__ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/build")))
+if (process.env.NODE__ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")))
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'))
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
+  
+  app.use(errorHandler);
 app.listen(port, async () => {
   await connectDB();
   console.log(`server on ${port}`);
