64cf07b5b34f14254e84a133fcf3f23fa1c120d0
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-01 12:07:18 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,18 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/goals", require("./routes/goalRoutes"));
 
 //server Frontend
-if (process.env.NODE__ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE__ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
   );
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
-  
-  app.use(errorHandler);
+app.use(errorHandler);
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
