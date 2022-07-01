e0297d9063447b0a9006bfbc986ebf1cc932e400
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-01 16:24:21 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/goals", require("./routes/goalRoutes"));
 
+
+app.use('/api', require(path.join(__dirname, 'routes', 'goalRoutes.js', 'userRoutes.js')))
 // Serve frontend
-if (process.env.NODE_ENV == "production") {
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('/*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    );
-  });
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+      path.join(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} 
 
-app.use(errorHandler);
+  
+  app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+  app.listen(port, () => console.log(`Server started on port ${port}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-    <Router>
+    <Router basename='/'>
       <div className='container'>
       <Header />
       <Routes>
