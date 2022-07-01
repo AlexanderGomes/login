f82856f4afaf3fca349a03dfda7eec624a8af652
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-01 15:18:15 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,12 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
+  app.get('/', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
   );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+} 
 
   
   app.use(errorHandler);
