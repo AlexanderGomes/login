8f260ed79a831cfa431dec6fc47e92cd30799530
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-06-30 19:19:51 -0700

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <div className='container'>
       <Header />
       <Routes>
-        <Route  path='/DIRE' element={<Dashboard />}/>
+        <Route  path='/' element={<Dashboard />}/>
         <Route  path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />} />
       </Routes>
