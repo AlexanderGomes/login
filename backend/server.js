aa7db68c7f44135ad063e97a32af7a495980f75e
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-01 12:21:01 -0700

    done

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const path = require("path");
 const app = express();
 
 connectDB();
@@ -16,7 +15,7 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 
 //server Frontend
 if (process.env.NODE__ENV === "production") {
-  app.use(express.static(path.join( "../frontend/build")));
+  app.use(express.static( "../frontend/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(
