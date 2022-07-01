8495f6bf8a838ebfaedb01559ad56bc64009d8be
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-01 12:52:02 -0700

    done

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,15 +6,12 @@ const {
     setGoal,
     updateGoal,
     deleteGoal,
-    like__deslike__post,
-    getTimelinePosts,
     getAllGoals
   } = require('../controllers/goalController')
 
 
   const { protect } = require('../middleware/authMiddleware')
-router.route('/:id/like').put(like__deslike__post)
-router.route('/timeline/all').get(getTimelinePosts)
+
 router.route('/').get(protect, getGoals).post(protect, setGoal)
 router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 router.get('/all', getAllGoals)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,10 +8,6 @@ router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/all',all )
 router.get('/me', protect, getMe)
-router.route('/:id/follow').put(followUser)
-router.route('/:id/unfollow').put(unfollowUser)
-router.route('/:id/update').put(updateUser)
-router.get('/:id/single', getUser)
 
 
 module.exports = router
