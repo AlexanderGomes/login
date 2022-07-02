0e6f5c4850477be6043af431d05c7aa784803d68
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-02 14:36:10 -0700

    done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -44,6 +44,7 @@ function Dashboard() {
       <section className='heading'>
         <h1>Welcome {user && user.name}</h1>
         <p>Nobody will know what you said 🤫</p>
+         <p>hello </p>
       </section>
 
       <GoalForm />
